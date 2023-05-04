df193606210b7847b362554edadba7c1be42baa9
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2023-05-04 08:23:59 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (43.49%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,16 +11,36 @@
  */
 class Solution {
 public:
-    vector<int> ans;
-    void dfs(TreeNode* root){
-        if(root==NULL)
-            return;  
-        dfs(root->left); 
-        dfs(root->right);
-        ans.push_back(root->val);
-    }
+    // vector<int> ans;
+    // void dfs(TreeNode* root){
+    //     if(root==NULL)
+    //         return;  
+    //     dfs(root->left); 
+    //     dfs(root->right);
+    //     ans.push_back(root->val);
+    // }
     vector<int> postorderTraversal(TreeNode* root) {
-        dfs(root);
+        vector<int> ans; 
+        if(root==NULL)
+            return ans;
+        
+        stack<TreeNode*> st1; 
+        stack<TreeNode*> st2;
+        st1.push(root); 
+        while(st1.size()!=0){
+            TreeNode *temp=st1.top();
+            st2.push(temp);
+            st1.pop(); 
+            if(temp->left!=NULL)
+                st1.push(temp->left);
+            if(temp->right!=NULL)
+                st1.push(temp->right);
+        } 
+        
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val); 
+            st2.pop();
+        } 
         return ans;
     }
 };
