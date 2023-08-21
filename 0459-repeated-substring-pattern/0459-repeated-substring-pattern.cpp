43a699d185484425c98976f66fdd5b5f78a8adf7
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2023-08-21 09:18:45 +0530

    Time: 262 ms (13.45%), Space: 127.3 MB (13.83%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    void findDiv(vector<int> &div,int n){
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                div.push_back(i);
+            }
+        }
+    }
+
+    bool repeatedSubstringPattern(string s) {
+
+        unordered_map<char,int> ump; 
+        for(auto it:s){
+            ump[it]++;
+        }
+        if(ump.size()==1 && ump[s[0]]>1){  
+        return true; 
+        }
+        else if(ump.size()==1 && ump[s[0]]==1){
+            return false;
+        }
+
+        vector<int> div; 
+        int n=s.size();
+        findDiv(div,n); 
+
+        for(auto &it:div){
+            unordered_map<string,int> ump;
+            for(int i=0;i<=n-it;i=i+it){
+                string t=s.substr(i,it); 
+                ump[t]++;
+            }
+            if(ump.size()==1)
+            return true;
+        }
+
+        return false;
+
+    }
+};
