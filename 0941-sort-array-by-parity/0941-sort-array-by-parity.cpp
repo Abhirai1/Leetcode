6211f28f270dee9124a3e229a4e16301c595b139
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2023-09-28 10:24:39 +0530

    Time: 10 ms (32.57%), Space: 16.5 MB (25.63%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,18 +1,50 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int>ans; 
-        int i; 
-        for(i=0;i<nums.size();i++){
-            if(nums[i]%2==0){
-                ans.push_back(nums[i]);
+        if(nums.size()==1){
+            return nums;
+        }
+
+        int odd=-1; 
+        int even=-1;
+
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if((nums[i]%2) !=0){
+                odd=i; 
+                break;
             }
         }
-        for(i=0;i<nums.size();i++){
-            if(nums[i]%2!=0){
-                ans.push_back(nums[i]);
+
+        for(int i=n-1;i>=0;i--){
+            if((nums[i]%2)==0){
+                // cout<<"hii"<<endl;
+                even=i; 
+                break;
             }
-        } 
-        return ans;
+        }
+
+        if(odd==-1 || even==-1)
+        return nums;
+
+        cout<<"odd -> "<<odd<<" even -> "<<even<<endl;
+
+        while(odd<even){
+            if((nums[odd]%2!=0 && nums[even]%2==0)){
+                 swap(nums[odd],nums[even]);
+                 odd++,even--;
+            }
+           else if(nums[even]%2!=0){
+                even--;
+            }
+            else if(nums[odd]%2==0){
+                odd++;
+            }
+            
+           
+        }
+
+        return nums;
+
     }
 };
