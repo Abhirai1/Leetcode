0f2c744ffd978b000d0b8330d945873f854a394e
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2023-10-17 08:13:44 +0530

    Time: 0 ms (100%), Space: 6.3 MB (55.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:  
    
-    // int cnt(int n,int dp[]){
-    //     if(n<=1){ 
-    //         return 1;
-    //     }
-    //     if(dp[n]!=-1)
-    //     return dp[n]; 
-
-    //     return dp[n]=(cnt(n-1,dp)+ cnt(n-2,dp));
-
-    // }
-    int climbStairs(int n) {
-        if(n<=1){
+    int cnt(int n,int dp[]){
+        if(n<=1){ 
             return 1;
         }
+        if(dp[n]!=-1)
+        return dp[n]; 
+
+        return dp[n]=(cnt(n-1,dp)+ cnt(n-2,dp));
+
+    }
+    int climbStairs(int n) {
+    //     if(n<=1){
+    //         return 1;
+    //     }
         int dp[n+1];
         memset(dp,-1,sizeof dp);
-       int lp=1;
-       int p=1;
-       int ans=0;
-        for(int i=2;i<=n;i++){
-            ans=p+lp; 
-            lp=p;
-            p=ans;
-        }
-        // cnt(n,dp);
+    //    int lp=1;
+    //    int p=1;
+    //    int ans=0;
+    //     for(int i=2;i<=n;i++){
+    //         ans=p+lp; 
+    //         lp=p;
+    //         p=ans;
+    //     }
+        // 
 
-        return ans;
+        return cnt(n,dp);;
     }
 };
