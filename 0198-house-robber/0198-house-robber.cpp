051957e00755c12907d5fc9f222efc771f0ee8c4
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2023-10-17 22:26:22 +0530

    Time: 3 ms (36.62%), Space: 8.1 MB (35.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-   int t[1001];
+//    int t[1001];
    
-    int solve(vector<int> &nums,int pos){
-        if(pos>=nums.size()){
-            return 0;
-        }
+//     int solve(vector<int> &nums,int pos){
+//         if(pos>=nums.size()){
+//             return 0;
+//         }
 
-        if(t[pos]!=-1) 
-        return t[pos];
+//         if(t[pos]!=-1) 
+//         return t[pos];
 
-        int in =nums[pos]+solve(nums,pos+2);
-        int ex =solve(nums,pos+1);
+//         int in =nums[pos]+solve(nums,pos+2);
+//         int ex =solve(nums,pos+1);
          
-        return t[pos]=max(ex,in);
-    }
+//         return t[pos]=max(ex,in);
+//     }
     int rob(vector<int>& nums) {
+        int t[1001];
         memset(t,-1,sizeof(t));
-        int i=0;
-        solve(nums,i);
-        // for(auto it:t)
-        // cout<<it<<" ";        
-        return t[0];
+        t[0]=0;
+        t[1]=nums[0];
+        for(int i=2;i<=nums.size();i++){
+            int inc=nums[i-1] + t[i-2];
+            int exc=t[i-1];
+            t[i]=max(inc,exc);
+        }    
+        return t[nums.size()];
     }
 };
