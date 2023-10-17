b7d75f88fa721d28280b2dcead1d6c1d2a0b390e
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2023-10-17 21:51:23 +0530

    Time: 0 ms (100%), Space: 7.9 MB (63.57%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+   int t[1001];
+   
+    int solve(vector<int> &nums,int pos){
+        if(pos>=nums.size()){
+            return 0;
+        }
+
+        if(t[pos]!=-1) 
+        return t[pos];
+
+        int in =nums[pos]+solve(nums,pos+2);
+        int ex =solve(nums,pos+1);
+         
+        return t[pos]=max(ex,in);
+    }
+    int rob(vector<int>& nums) {
+        memset(t,-1,sizeof(t));
+        int i=0;
+        
+        return solve(nums,i);
+    }
+};
