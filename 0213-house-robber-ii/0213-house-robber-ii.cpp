405e3b33ba2f3926f6a356c8de5c2c1f77b84af9
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2023-10-20 21:20:56 +0530

    Time: 0 ms (100%), Space: 8.2 MB (28.33%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
+
     int t[101];
     int bottomUp(vector<int> ans,int s,int n){
+
         memset(t,-1,sizeof(t));
         t[0]=0;
         t[1]=ans[s];
+
         for(int i=2;i<=n;i++){
             int pick=ans[i-1]+t[i-2];
             int notpick=t[i-1];
             t[i]=max(pick,notpick);
         }
+
         return t[n];
+
     }
     int rob(vector<int>& nums) {
         
@@ -20,14 +25,15 @@ public:
         if(nums.size()==2){
             return max(nums[0],nums[1]);
         }
+
         vector<int> temp=nums;
         int n=nums.size(); 
+
         temp.erase(temp.begin());
         int notpick=bottomUp(temp,0,n-1);
         nums.pop_back();
         int pick=bottomUp(nums,0,n-1);  
 
         return max(pick,notpick);
-
     }
 };
