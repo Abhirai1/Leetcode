2e83d53c0592d36b9a52a9f141ebead5799f6a91
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2023-10-22 11:58:58 +0530

    Time: 128 ms (89.85%), Space: 93.4 MB (78.96%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
-    long long int t[100006][2];
-    long long int  solve(vector<int>& nums,int ind,int cnt){
+    // long long int t[100006][2];
+    // long long int  solve(vector<int>& nums,int ind,int cnt){
 
-        if(ind==nums.size()){
-            return 0;
-        } 
+    //     if(ind==nums.size()){
+    //         return 0;
+    //     } 
 
-        if(t[ind][cnt]!=-1) return t[ind][cnt];
+    //     if(t[ind][cnt]!=-1) return t[ind][cnt];
 
-        long long int  skip= solve(nums,ind+1,cnt);
-        long long int take;
-        if(cnt==0)
-        take=nums[ind]+solve(nums,ind+1,!cnt);
-        else
-        take=((-1)*nums[ind])+solve(nums,ind+1,!cnt);
+    //     long long int  skip= solve(nums,ind+1,cnt);
+    //     long long int take;
+    //     if(cnt==0)
+    //     take=nums[ind]+solve(nums,ind+1,!cnt);
+    //     else
+    //     take=((-1)*nums[ind])+solve(nums,ind+1,!cnt);
         
-        return t[ind][cnt] = max(skip,take);
-    }
+    //     return t[ind][cnt] = max(skip,take);
+    // }
 
     long long maxAlternatingSum(vector<int>& nums) {
-        int i=0;
-        // int cnt=0;
-        memset(t,-1,sizeof(t));
-        return solve(nums,i,0);
+        // int i=0;
+       long long int n=nums.size();
+       long long int t[100001][2];
+       long long int even=0;
+        long long int odd=1;
+        memset(t,0,sizeof(t));
+
+        t[0][even]=0;
+        t[0][odd]=0;
+        for(int i=1;i<=n;i++){
+            t[i][even]=max(t[i-1][odd]-nums[i-1],t[i-1][even]);
+            t[i][odd]=max(t[i-1][even]+nums[i-1],t[i-1][odd]);
+        }
+
+        return max(t[n][odd],t[n][even]);
     }
 };
