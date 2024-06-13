b441dbbe8a6ffe11d3170218bbf54a08bf63cec5
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2024-06-13 22:38:43 +0530

    Time: 8 ms (60.06%), Space: 8.3 MB (30.68%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,21 +4,23 @@ public:
         if(x<0){
             return false;
         }
-        long long int ans=0;
+        int ans=0;
         int n=x;
         while(n>0){
         int digit=n%10;
-        // if(INT_MAX/10<ans){
-        //     return false;
-        // }
+        if(INT_MAX/10<ans){
+            return false;
+        }
         ans=ans*10+digit;
         n=n/10;
         }
         
         if(ans==x){
-        return true;
+return true;
         }
+        else{
         return false;
+        }
     }
    
 };
