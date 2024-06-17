09caa4ce001da1007721973bbe4984aeb96ffb50
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2024-06-17 23:26:52 +0530

    Time: 36 ms (51.97%), Space: 16.1 MB (59.99%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
 
-    // let's analyse time complexity
-
-    int t[10001];
-    int solve(vector<int>& coins,int amount){
-        if(amount==0) return 0; 
-        if(amount<0) return 100000;
-
-        if(t[amount]!=-1) return t[amount];
-
-        int take=100000;
-        for(auto it:coins){
-            if(it<=amount){
-                take=min(1+solve(coins,amount-it),take);
-            }
-        }
-
-        return t[amount]=take;
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        
-        int t[amount+1];
-        t[0]=0; 
-        for(int i=1;i<=amount;i++) t[i]=100000; 
-
-        for(int amt=1;amt<=amount;amt++){
-            for(auto it:coins){
-                if(it<=amt){
-                    t[amt] = min(1+t[amt-it],t[amt]);
+        int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++){
+            for(int j = 0; j < coins.size(); j++){
+                if(i - coins[j] >= 0){
+                    if(dp[i - coins[j]] != INT_MAX){
+                        dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+                    }
                 }
             }
         }
-
-        return t[amount]==100000 ? -1 : t[amount];
+        if(dp[amount] == INT_MAX) return -1;
+        return dp[amount];
     }
 };
