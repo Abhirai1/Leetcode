4bf277863008abefd3701f6e079df25a9a2358aa
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2024-06-17 23:56:12 +0530

    Time: 2501 ms (5.02%), Space: 418.3 MB (13.75%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -4,12 +4,13 @@ public:
     int helper(string s, string t, int i, int j, vector<vector<int>> &dp){
         if(j == t.size()) return dp[i][j] = 1;
         if(i == s.size()) return dp[i][j] = 0;
+
         if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] != t[j]){
-            return dp[i][j] = helper(s,t,i+1,j,dp);
-        }
+        if(s[i] != t[j]) return dp[i][j] = helper(s,t,i+1,j,dp);
+        
         int pick = helper(s,t,i + 1, j + 1,dp);
         int not_pick = helper(s,t,i + 1, j,dp);
+        
         return dp[i][j] = (pick + not_pick) % mod;
     }
     int numDistinct(string s, string t) {
