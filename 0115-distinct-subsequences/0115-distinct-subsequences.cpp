8e166ae7142cb5b12307c03a57cc3846b898b7fb
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2024-06-17 23:51:10 +0530

    Time: 2581 ms (5.02%), Space: 418.2 MB (13.75%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mod = pow(10,9) + 7;
+    int helper(string s, string t, int i, int j, vector<vector<int>> &dp){
+        if(j == t.size()) return dp[i][j] = 1;
+        if(i == s.size()) return dp[i][j] = 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s[i] != t[j]) return dp[i][j] = helper(s,t,i+1,j,dp);
+        
+        int pick = helper(s,t,i + 1, j + 1,dp);int not_pick = helper(s,t,i + 1, j,dp);
+        return dp[i][j] = (pick + not_pick) % mod;
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size(), m = t.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return helper(s,t,0,0,dp);
+    }
+};
