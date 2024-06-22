359d53c09fd4bb1792c4cfd877c16c63f0bff742
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2024-06-22 10:40:57 +0530

    Time: 125 ms (23.34%), Space: 50.2 MB (13.8%) - LeetHub

--- 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.cpp
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.cpp
@@ -1,23 +1,28 @@
+#include <unordered_map>
+#include <set>
+
 class Solution {
 public:
-    int t[1000000];
-    int power(int n){
-        if(n == 1) return 0;
-        if(t[n]!=-1) return t[n];
-        if(n % 2 == 0) return t[n] = 1 + power(n / 2);
-        return t[n] = 1 + power(3 * n + 1);
+    int power(int n, std::unordered_map<int, int>& dp) {
+        if (n == 1) return 0;
+        if (dp.find(n) != dp.end()) return dp[n];
+        if (n % 2 == 0) return dp[n] = 1 + power(n / 2, dp);
+        return dp[n] = 1 + power(3 * n + 1, dp);
     }
+
     int getKth(int lo, int hi, int k) {
-        memset(t,-1,sizeof(t));
-        map<int,int> dp;
-        multimap<int,int> m;        
-        for(int i = lo; i <= hi; i++){
-            int val = power(i);
-            m.insert({val,i});
+        std::unordered_map<int, int> dp;
+        // Use a set of tuples to enforce secondary sorting by the integer value
+        std::set<std::pair<int, int>> s;
+        
+        for (int i = lo; i <= hi; i++) {
+            int val = power(i, dp);
+            s.insert({val, i});
         }
-        for(auto it : m){
+        
+        for (const auto& it : s) {
             k--;
-            if(k == 0) return it.second;
+            if (k == 0) return it.second;
         }
         return -1;
     }
