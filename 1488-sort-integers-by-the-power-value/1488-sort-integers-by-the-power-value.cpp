b682e9d44005dc2b6c3a4eb7670add9027a98175
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2024-06-22 08:25:12 +0530

    Time: 76 ms (41.41%), Space: 18.6 MB (45.29%) - LeetHub

--- 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.cpp
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-        int power(int n, map<int,int> &dp){
+    int t[1000000];
+    int power(int n){
         if(n == 1) return 0;
-        if(dp.find(n) != dp.end()) return dp[n];
-        if(n % 2 == 0) return dp[n] = 1 + power(n / 2, dp);
-        return dp[n] = 1 + power(3 * n + 1, dp);
+        if(t[n]!=-1) return t[n];
+        if(n % 2 == 0) return t[n] = 1 + power(n / 2);
+        return t[n] = 1 + power(3 * n + 1);
     }
     int getKth(int lo, int hi, int k) {
+        memset(t,-1,sizeof(t));
         map<int,int> dp;
         multimap<int,int> m;        
         for(int i = lo; i <= hi; i++){
-            int val = power(i,dp);
+            int val = power(i);
             m.insert({val,i});
         }
         for(auto it : m){
