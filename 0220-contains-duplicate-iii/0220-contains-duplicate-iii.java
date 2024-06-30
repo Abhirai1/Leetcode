5127fcab9debee076c71c01774233961d3be096b
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2024-06-30 21:57:52 +0530

    Time: 111 ms (68.68%), Space: 62 MB (31.22%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
@@ -0,0 +1,29 @@
+import java.util.TreeSet;
+
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        if (nums == null || nums.length < 2 || indexDiff < 1 || valueDiff < 0) {
+            return false;
+        }
+
+        TreeSet<Long> set = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            Long num = (long) nums[i];
+
+            // Find the smallest number >= nums[i] - valueDiff
+            Long floor = set.floor(num + valueDiff);
+            if (floor != null && floor >= num - valueDiff) {
+                return true;
+            }
+
+            set.add(num);
+
+            // Maintain the size of the set to be at most indexDiff
+            if (i >= indexDiff) {
+                set.remove((long) nums[i - indexDiff]);
+            }
+        }
+
+        return false;
+    }
+}
