a7732051b19ba469171262b43dbe1874b51ac72d
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2024-07-13 12:09:15 +0530

    Time: 229 ms (52.67%), Space: 206.4 MB (64.5%) - LeetHub

---
+++ 2846-robot-collisions/2846-robot-collisions.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        vector<int> ans; 
+
+        int n=positions.size();
+
+        stack<pair<int,int>> st; 
+
+        vector<pair<int,int>> ip; 
+
+        for(int i=0;i<n;i++) ip.push_back({positions[i],i});
+        
+        sort(ip.begin(),ip.end()); 
+
+        for(auto it:ip){
+            int pos=it.first; 
+            int ind=it.second; 
+            char dir=directions[ind]; 
+            // int hel=healths[ind]; 
+
+            // khalipan + dir same
+            if(st.size()==0 || dir==directions[st.top().second] || dir=='R'){
+                st.push({pos,ind});
+            }
+            else{
+                while(st.size()>0 && directions[st.top().second]=='R' && dir!=directions[st.top().second] && healths[ind]>healths[st.top().second]){
+                    healths[st.top().second]=0;
+                    healths[ind]--;
+                    st.pop();
+                }
+
+                if(st.size()>0 && directions[st.top().second]=='R' && dir!=directions[st.top().second] && healths[ind]==healths[st.top().second]){
+                    healths[st.top().second]=0;
+                    healths[ind]=0;
+                    st.pop();
+                    continue;
+                }
+
+                if(st.size()>0 && directions[st.top().second]=='R' && dir!=directions[st.top().second] && healths[ind]<healths[st.top().second]){
+                    healths[st.top().second]--;
+                    healths[ind]=0;
+                }
+
+                if((st.size()==0 && healths[ind]>0) || (st.size()>0 && dir==directions[st.top().second]))  st.push({pos,ind});
+
+            }
+        }
+
+        for(auto it:healths) if(it!=0) ans.push_back(it);
+
+        return ans;
+    }   
+};
