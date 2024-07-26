cb3c644b41092bb2aa91a1097bdad9c9ff2a83a9
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2024-07-26 11:43:50 +0530

    Time: 119 ms (77.5%), Space: 122 MB (99.33%) - LeetHub

--- 3454-minimum-operations-to-make-array-equal-to-target/3454-minimum-operations-to-make-array-equal-to-target.cpp
+++ 3454-minimum-operations-to-make-array-equal-to-target/3454-minimum-operations-to-make-array-equal-to-target.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     #define lli long long int
     long long minimumOperations(vector<int>& nums, vector<int>& target) {
-        lli n=nums.size();
-        lli curr=0,prev=0; 
-        lli ans=0;
+        lli n=nums.size();lli curr=0,prev=0;lli ans=0;
         for(lli i=0;i<n;i++){
             curr=target[i]-nums[i]; 
 
